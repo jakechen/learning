d39867568cf1a64723ef192df305bc4f6e36fe89
Jake Chen <jake@jakechen.com>
2016-09-08 09:34:39 -0500

    dynamic route

--- RealPython/flask-hello-world/app.py
+++ RealPython/flask-hello-world/app.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
-
-# define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>") # take a query parameter
+def search(search_query):
+    return search_query
 
 # start the development server using the run() method
 if __name__=="__main__":
