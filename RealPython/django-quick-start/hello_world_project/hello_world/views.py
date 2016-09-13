3d9574252c29836f34cabc222988a88af128ba0c
Jake Chen <jake@jakechen.com>
2016-09-13 08:22:23 -0500

    betterhello

--- RealPython/django-quick-start/hello_world_project/hello_world/views.py
+++ RealPython/django-quick-start/hello_world_project/hello_world/views.py
@@ -1,3 +1,5 @@
+from django.template import Context, loader
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,3 +10,8 @@ def about(request):
     return HttpResponse(
         "Here is the About Page. Want to return home? <a href='/'>Back Home</a>"
     )
+
+def better(request):
+    t = loader.get_template('betterhello.html')
+    c = Context({'current_time':datetime.now()})
+    return HttpResponse(t.render(c))
